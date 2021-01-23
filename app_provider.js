8bd7a35987b27d6dd0a64e341022c1aaa0023e71
Alexa <kozlovas317@gmail.com>
2021-01-23 21:21:22 +0100

    renamed method

--- app_provider.js
+++ app_provider.js
@@ -3,9 +3,8 @@ const require = createRequire(import.meta.url);
 const fs = require('fs');
 
 
-export function processFile() {
+export function getApplications() {
     let rawdata = fs.readFileSync('app_config.json');
     let applications = JSON.parse(rawdata);
-    console.log(applications[0]); 
     return applications;
 }
