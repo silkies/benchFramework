7c79bef613396d137b82f0c6ecf507d63defc14c
Alexa <kozlovas317@gmail.com>
2021-03-13 16:07:29 +0100

    added input selector

--- workflow_provider.js
+++ workflow_provider.js
@@ -2,10 +2,17 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const fs = require('fs');
 
-
-export function getSelector() {
+function processConfigFile() {
     let rawdata = fs.readFileSync('workflow_config.json');
     let data = JSON.parse(rawdata);
-
+    return data;
+}
+export function getPageSelector() {
+    let data = processConfigFile();
     return data.selector;
+}
+
+export function getInputSelector() {
+    let data = processConfigFile();
+    return data.input;
 }
